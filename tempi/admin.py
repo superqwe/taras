c3ddf7a397d89f62e0a3dfacd25ef82a5d0d1d15
superqwe <superqwe@users.noreply.github.com>
2018-06-25 18:04:36 +0200

    modificati modelli

--- tempi/admin.py
+++ tempi/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Tempo, Squadra
+from .models import Tempo, Squadra, Barca
 
 # Register your models here.
 admin.site.register(Tempo)
+admin.site.register(Barca)
 admin.site.register(Squadra)
--- tempi/models.py
+++ tempi/models.py
@@ -1,15 +1,48 @@
 from django.db import models
 
+MF = (
+    ('M', 'Maschile'),
+    ('F', 'Femminile'),
+)
+
+TURNO = (
+    ('M', 'Mattina'),
+    ('P', 'Pomeriggio')
+)
+
 
 # Create your models here.
 
 class Squadra(models.Model):
     nome = models.CharField(max_length=200)
-    mf = models.CharField(max_length=1)
+    mf = models.CharField('Maschile/Femminile', max_length=1,
+                          choices=MF,
+                          default='M'
+                          )
+
+    class Meta:
+        ordering = ['nome', '-mf']
+        verbose_name_plural = 'Squadre'
+
+
+class Barca(models.Model):
+    nome = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['nome']
+        verbose_name_plural = 'Barche'
+
 
 class Tempo(models.Model):
-    n = models.IntegerField()
-    turno = models.IntegerField()
+    n = models.IntegerField(default=1)
+    turno = models.CharField(max_length=1,
+                             choices=TURNO,
+                             default='M'
+                             )
     squadra = models.ForeignKey(Squadra, on_delete=models.CASCADE)
-    barca = models.CharField(max_length=200)
-    tempo = models.TimeField()
+    barca = models.ForeignKey(Barca, on_delete=models.CASCADE)
+    tempo = models.DurationField()
+
+    class Meta:
+        ordering = ['n', 'turno']
+        verbose_name_plural = 'Tempi'
