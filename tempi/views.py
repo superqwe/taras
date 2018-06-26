b09ccacf3cffbd4fc7a97b733b031bee140cfe7e
superqwe <superqwe@users.noreply.github.com>
2018-06-26 23:29:46 +0200

    template gare.html

--- tempi/views.py
+++ tempi/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+from tempi.models import Tempo
+
+
 def index(request):
     return HttpResponse("ciaooo")
+
+
+def gare(request):
+    gare = Tempo.objects.all()
+    context = {'gare': gare}
+    return render(request, 'tempi/gare.html', context)
